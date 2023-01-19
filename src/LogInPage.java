9f36371708f49a64a82cfee3e25e79e680909b53
Luca Sergiu <sergiuluca2001@gmail.com>
2023-01-19 23:10:19 +0200

    SignUp page interface

--- src/LogInPage.java
+++ src/LogInPage.java
@@ -5,7 +5,7 @@ public class LogInPage {
     private JPanel panel1;
 
     public LogInPage() {
-        JFrame frame = new JFrame("SignUpPage");
+        JFrame frame = new JFrame("BD2 Project - Log In Page");
         frame.setContentPane(panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
--- src/SignUpPage.java
+++ src/SignUpPage.java
@@ -3,13 +3,18 @@ import javax.swing.*;
 public class SignUpPage {
 
     private JPanel panel1;
-    private JTextField emailField;
+    private JPanel centerPanel;
+    private JTextField textField1;
+    private JLabel passwordLabel;
+    private JPasswordField passwordField1;
     private JLabel emailLabel;
-    private JLabel passlabel;
-    private JPasswordField passField;
+    private JButton signUpButton;
+    private JButton haveAccountButton;
+    private JLabel accountTypeLabel;
+    private JComboBox accountTypeComboBox;
 
     public SignUpPage() {
-        JFrame frame = new JFrame("SignUpPage");
+        JFrame frame = new JFrame("BD2 Project - Sign Up Page");
         frame.setContentPane(panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
