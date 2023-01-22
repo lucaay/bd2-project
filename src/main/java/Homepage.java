19a1f88ffa1b28467ef0f04a35530de6055042b7
Luca Sergiu <sergiuluca2001@gmail.com>
2023-01-22 02:48:59 +0200

    show properties based on selected product

--- src/main/java/Homepage.java
+++ src/main/java/Homepage.java
@@ -38,7 +38,7 @@ public class Homepage {
     private JLabel nameLabel;
     private JLabel componentTypeLabel;
     private JLabel chipsetMakerLabel;
-    private JLabel cpuSocketLabel;
+    private JLabel socketLabel;
     private JLabel memoryTypeLabel;
     private JLabel maxMemoryLabel;
     private JLabel numberOfSlotsLabel;
@@ -60,7 +60,7 @@ public class Homepage {
     private JLabel ssdTypeLabel;
     private JLabel maxReadLabel;
     private JLabel maxWriteLabel;
-    private JLabel modLabel;
+    private JLabel modulationLabel;
     private JLabel memorySizeLabel;
     private JLabel memoryEfectiveFreqLabel;
     private JTextField memoryEfectiveFreqField;
@@ -77,7 +77,144 @@ public class Homepage {
     private JTextField freqField;
 
 
+    private void hideAllProperties() {
+        nameField.setVisible(false);
+        nameLabel.setVisible(false);
+        componentTypeLabel.setVisible(false);
+        chipsetMakerLabel.setVisible(false);
+        socketLabel.setVisible(false);
+        memoryTypeLabel.setVisible(false);
+        maxMemoryLabel.setVisible(false);
+        numberOfSlotsLabel.setVisible(false);
+        memoryFreqLabel.setVisible(false);
+        seriesLabel.setVisible(false);
+        powerLabel.setVisible(false);
+        capacityLabel.setVisible(false);
+        ssdTypeLabel.setVisible(false);
+        maxReadLabel.setVisible(false);
+        maxWriteLabel.setVisible(false);
+        modulationLabel.setVisible(false);
+        memorySizeLabel.setVisible(false);
+        memoryEfectiveFreqLabel.setVisible(false);
+        coolingSystemLabel.setVisible(false);
+        nameField.setVisible(false);
+        memorySizeField.setVisible(false);
+        maxWriteField.setVisible(false);
+        maxReadField.setVisible(false);
+        capacityField.setVisible(false);
+        powerField.setVisible(false);
+        numberOfCoresLabel.setVisible(false);
+        freqLabel.setVisible(false);
+        numberOfCoresField.setVisible(false);
+        freqField.setVisible(false);
+        componentTypeComboBox.setVisible(false);
+        chipsetComboBox.setVisible(false);
+        memoryTypeComboBox.setVisible(false);
+        modulationCheckBox.setVisible(false);
+        ssdTypeComboBox.setVisible(false);
+        coolingSystemComboBox.setVisible(false);
+        socketField.setVisible(false);
+        maxMemoryField.setVisible(false);
+        slotsNumberField.setVisible(false);
+        memoryFreqField.setVisible(false);
+        seriesField.setVisible(false);
+        memoryEfectiveFreqField.setVisible(false);
+    }
+
+    private void showProperties(String componentType) {
+        hideAllProperties();
+        nameField.setVisible(true);
+        nameLabel.setVisible(true);
+        componentTypeLabel.setVisible(true);
+        componentTypeComboBox.setVisible(true);
+        switch (componentType) {
+            case "Placa de baza":
+                chipsetMakerLabel.setVisible(true);
+                chipsetComboBox.setVisible(true);
+                socketLabel.setVisible(true);
+                socketField.setVisible(true);
+                memoryTypeLabel.setVisible(true);
+                memoryTypeComboBox.setVisible(true);
+                maxMemoryLabel.setVisible(true);
+                maxMemoryField.setVisible(true);
+                numberOfSlotsLabel.setVisible(true);
+                slotsNumberField.setVisible(true);
+                memoryFreqLabel.setVisible(true);
+                memoryFreqField.setVisible(true);
+                break;
+            case "Memorie RAM":
+                seriesLabel.setVisible(true);
+                seriesField.setVisible(true);
+                memoryTypeLabel.setVisible(true);
+                memoryTypeComboBox.setVisible(true);
+                capacityLabel.setVisible(true);
+                capacityField.setVisible(true);
+                freqLabel.setVisible(true);
+                freqField.setVisible(true);
+                break;
+            case "Stocare SSD":
+                seriesLabel.setVisible(true);
+                seriesField.setVisible(true);
+                ssdTypeLabel.setVisible(true);
+                ssdTypeComboBox.setVisible(true);
+                capacityLabel.setVisible(true);
+                capacityField.setVisible(true);
+                maxReadLabel.setVisible(true);
+                maxReadField.setVisible(true);
+                maxWriteLabel.setVisible(true);
+                maxWriteField.setVisible(true);
+                break;
+            case "Procesor":
+                socketLabel.setVisible(true);
+                socketField.setVisible(true);
+                seriesLabel.setVisible(true);
+                seriesField.setVisible(true);
+                numberOfCoresLabel.setVisible(true);
+                numberOfCoresField.setVisible(true);
+                freqLabel.setVisible(true);
+                freqField.setVisible(true);
+                powerLabel.setVisible(true);
+                powerField.setVisible(true);
+                memoryTypeLabel.setVisible(true);
+                memoryTypeComboBox.setVisible(true);
+                maxMemoryLabel.setVisible(true);
+                maxMemoryField.setVisible(true);
+                memoryFreqLabel.setVisible(true);
+                memoryFreqField.setVisible(true);
+
+                break;
+            case "Placa video":
+                chipsetMakerLabel.setVisible(true);
+                chipsetComboBox.setVisible(true);
+                memoryTypeLabel.setVisible(true);
+                memoryTypeComboBox.setVisible(true);
+                memoryEfectiveFreqLabel.setVisible(true);
+                memoryEfectiveFreqField.setVisible(true);
+                seriesLabel.setVisible(true);
+                seriesField.setVisible(true);
+                powerLabel.setVisible(true);
+                powerField.setVisible(true);
+                memorySizeLabel.setVisible(true);
+                memorySizeField.setVisible(true);
+                coolingSystemLabel.setVisible(true);
+                coolingSystemComboBox.setVisible(true);
+                break;
+            case "Sursa":
+                modulationLabel.setVisible(true);
+                modulationCheckBox.setVisible(true);
+                powerLabel.setVisible(true);
+                powerField.setVisible(true);
+
+                break;
+            default:
+                System.out.println("Invalid component type");
+        }
+    }
+
     public Homepage(ApplicationInterface applicationInterface) {
+        hideAllProperties();
+//        showProperties(componentTypeComboBox.getSelectedItem().toString()); // show properties for the selected component type
+        showProperties("Stocare SSD");
         backToLoginPage.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
