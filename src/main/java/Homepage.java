971ec74c14929043aaf9272aac391909d859b97b
Luca Sergiu <sergiuluca2001@gmail.com>
2023-01-22 02:21:33 +0200

    product details interface

--- src/main/java/Homepage.java
+++ src/main/java/Homepage.java
@@ -34,6 +34,47 @@ public class Homepage {
     private JScrollPane dataScrollPane;
     private JTable dataTable;
     private JLabel propertiesTitleLabel;
+    private JTextField nameField;
+    private JLabel nameLabel;
+    private JLabel componentTypeLabel;
+    private JLabel chipsetMakerLabel;
+    private JLabel cpuSocketLabel;
+    private JLabel memoryTypeLabel;
+    private JLabel maxMemoryLabel;
+    private JLabel numberOfSlotsLabel;
+    private JLabel memoryFreqLabel;
+    private JLabel dataPanelTitleLabel;
+    private JComboBox componentTypeComboBox;
+    private JComboBox chipsetComboBox;
+    private JComboBox memoryTypeComboBox;
+    private JCheckBox modulationCheckBox;
+    private JComboBox ssdTypeComboBox;
+    private JComboBox coolingSystemComboBox;
+    private JTextField socketField;
+    private JTextField maxMemoryField;
+    private JTextField slotsNumberField;
+    private JTextField memoryFreqField;
+    private JLabel seriesLabel;
+    private JLabel powerLabel;
+    private JLabel capacityLabel;
+    private JLabel ssdTypeLabel;
+    private JLabel maxReadLabel;
+    private JLabel maxWriteLabel;
+    private JLabel modLabel;
+    private JLabel memorySizeLabel;
+    private JLabel memoryEfectiveFreqLabel;
+    private JTextField memoryEfectiveFreqField;
+    private JLabel coolingSystemLabel;
+    private JTextField seriesField;
+    private JTextField memorySizeField;
+    private JTextField maxWriteField;
+    private JTextField maxReadField;
+    private JTextField capacityField;
+    private JTextField powerField;
+    private JLabel numberOfCoresLabel;
+    private JLabel freqLabel;
+    private JTextField numberOfCoresField;
+    private JTextField freqField;
 
 
     public Homepage(ApplicationInterface applicationInterface) {
